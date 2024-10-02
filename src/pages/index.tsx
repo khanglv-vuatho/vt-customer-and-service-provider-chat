17251a2b899cc4dfffcf58c2200f135e93bf7740
khanglv-vuatho <khanglv@vuatho.com>
2024-10-02 16:29:49 +0700

    update Toast

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -214,10 +214,10 @@ const HomePage = () => {
       // seen all message in conversation when user get message
       if (data.status === 'SEEN MESSAGE') {
         if (data?.socket_id == socket?.id) return
-        ToastComponent({
-          type: 'success',
-          message: 'Bạn đã nhận được tin nhắn mới'
-        })
+        // ToastComponent({
+        //   type: 'success',
+        //   message: 'Bạn đã nhận được tin nhắn mới'
+        // })
         setConversation((prev) =>
           prev.map((message) => ({
             ...message,
